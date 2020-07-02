9c750e8ecf0a26eee6dacf82ff00be3442c31cd0
Q. Price <priceqp@gmail.com>
2020-07-02 09:17:23 -0500

    initial commit

---
+++ queries_products.sql
@@ -0,0 +1,40 @@
+-- Add a product to the table with the name of “chair”, price of 44.00, and can_be_returned of false.
+
+
+-- Add a product to the table with the name of “stool”, price of 25.99, and can_be_returned of true.
+
+
+-- Add a product to the table with the name of “table”, price of 124.00, and can_be_returned of false.
+
+
+-- Display all of the rows and columns in the table.
+
+
+-- Display all of the names of the products.
+
+
+-- Display all of the names and prices of the products.
+
+
+-- Add a new product - make up whatever you would like!
+
+
+-- Display only the products that can_be_returned.
+
+
+-- Display only the products that have a price less than 44.00.
+
+
+-- Display only the products that have a price in between 22.50 and 99.99.
+
+
+-- There’s a sale going on: Everything is $20 off! Update the database accordingly.
+
+
+-- Because of the sale, everything that costs less than $25 has sold out. Remove all products whose price meets this criteria.
+
+
+-- And now the sale is over. For the remaining products, increase their price by $20.
+
+
+-- There is a new company policy: everything is returnable. Update the database accordingly.
